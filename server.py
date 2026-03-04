a79b4e9644459056e6ac085bc12fe9c65464c478
Your Name <you@example.com>
2026-03-03 21:22:09 -0500

    test server

--- server.py
+++ server.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+from flow import handle_message
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Servidor funcionando"
+sessions = {}
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    return "OK"
 
+    phone = request.form.get("From")
+    message = request.form.get("Body")
+
+    if phone not in sessions:
+        sessions[phone] = {
+            "state": "START",
+            "cart": []
+        }
+
+    reply = handle_message(sessions[phone], message)
+
+    return f"""
+<Response>
+    <Message>{reply}</Message>
+</Response>
+"""
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
